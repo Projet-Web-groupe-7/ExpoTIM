5871ba9d66d1ffdc4d37e3ca9d855386a1facda3
massimo <118757668+massimo-trimboli@users.noreply.github.com>
2025-10-02 16:58:41 -0400

    linked style sheet

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 function theme_tp_enqueue_styles() { 
-wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
-wp_enqueue_style('main-style', get_stylesheet_uri()."style.css"); 
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
+    wp_enqueue_style('main-style', get_stylesheet_uri()); 
+    wp_enqueue_style('style', get_template_directory_uri() . '/css/main.css'); 
 } 
 add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 ?>
