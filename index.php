4bbf517948f048c460d36b42ae07408c13df53b1
massimo <masst123543@gmail.com>
2025-10-07 15:25:37 -0400

    added index.php et change le gitignore

---
+++ index.php
@@ -0,0 +1,14 @@
+<p>front-page.php</p>
+<?php get_header(); ?>
+
+    <main>
+        <section class="projet-random">
+
+        </section>
+        <section class="decks">
+
+        </section>
+    </main>
+    <?php get_footer(); ?>
+</body>
+</html>
