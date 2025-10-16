d26ccf205bdc90a2e6a1045240d23b7566a7da9f
massimo <masst123543@gmail.com>
2025-10-15 22:16:53 -0400

    creer cartes

--- page-gallerie.php
+++ page-gallerie.php
@@ -18,13 +18,23 @@
 
         <section id="gallerie-cartes">
             <div class="paquet" style="color:white;">
-                <div class="carte">
+                
+            </div>
+            <div class="carte heart">
                     <div class="container">
-                        <div class="front"><h2>projet</h2><h3>1</h3></div>
+                        <div class="front">
+                            <div class="premier-etage">
+                                <p>Nom du projet</p>
+                                <div class="symbole"></div>
+                            </div>
+                            <img src="" alt="image du projet">
+                            <div class="symbole"></div>
+                        </div>
+
+
                         <div class="back"></div>
                     </div>
                 </div>
-            </div>
         </section>
     </main>
     
@@ -34,7 +44,7 @@
 
 <style>
     body{
-        background: linear-gradient(180deg,rgba(101, 126, 212, 1) 6%, rgba(178, 95, 126, 1) 61%, rgba(255, 56, 61, 1) 96%);
+        /* background: linear-gradient(180deg,rgba(101, 126, 212, 1) 6%, rgba(178, 95, 126, 1) 61%, rgba(255, 56, 61, 1) 96%); */
     }
     main.gallerie{
         padding: 0 50px;
@@ -51,5 +61,112 @@
 
 <!-- style pour les cartes -->
 <style>
-    
+.paquet{
+    width: 259px;
+    height: 388px;
+    border: 5px solid red;
+    background-color: rgb(0, 52, 108);
+
+    z-index: 999;
+}
+
+
+.carte{
+    width: 259px;
+    height: 388px;
+    margin: 20px;
+    /* transition pour le déplacement */
+    transition: transform .7s;
+}
+/* le container est ce qui tourne */
+.carte .container{
+    width: 100%;
+    height: 100%;
+
+    transform-style: preserve-3d;
+    transform: rotate3d(0, 0, 0, 180deg);
+    transition: transform .7s;
+}
+/* .carte:hover .container{
+    transform: rotate3d(0, 1, 0, 180deg);
+} */
+.carte.turned .container{
+    transform: rotate3d(0, 1, 0, 180deg);
+}
+
+
+
+/* /////////////////////////////////////////////////////// design de la carte */
+/* devant et arriere de la carte */
+.carte .back,.front{
+    width: 100%;
+    height: 100%;
+    border: 6px solid red;
+    border-radius: 20px;
+
+    backface-visibility: hidden;
+    /* position: absolute; */
+}
+.carte .back{
+    background-color: rgb(24, 130, 200);
+}
+.carte .front{
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+    align-items: center;
+    font-family: var(--main-paragraphe-font);
+
+    /* padding: 15px 20px; */
+
+    background-color: var(--carte-couleur-fond);
+
+    /* transform: rotate3d(0, 1, 0, 180deg); */
+}
+.carte .front .premier-etage{
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0;
+}
+.carte .front .premier-etage p,div{
+    margin: 20px 20px 0 20px;
+}
+.carte .front img{
+    width: 233px;
+    height: 292px;
+    margin: 0 20px;
+    border-radius: 10px;
+    background-color: #D9D9D9;
+}
+.front .symbole{
+    width: 22px;
+    height: 24px;
+    background-size: contain;
+    background-position: center;
+
+
+    /* background-image: url("hearts.png"); */
+    background-color:green;
+}
+.front .symbole:nth-of-type(2){
+    margin: 0 15px 15px 15px;
+    align-self: flex-start;
+}
+.carte.heart .container .front{
+    border: 6px solid linear-gradient(180deg,rgba(255, 51, 31, 1) 0%, rgba(248, 245, 245, 1) 51%, rgba(255, 51, 31, 1) 100%);
+}
+
+
+
+/* classes nécéssaires pour fonctoinement du code */
+.carte.hidden{
+    position: absolute;
+    opacity: 0;
+}
+.carte-no-transit{
+    transition: none;
+}
 </style>
