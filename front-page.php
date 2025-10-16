ecd7ddeb73ad41738d71e2d3c505c63f9d51e41b
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-10-16 17:11:19 -0400

    Update Erik + début css front-page

--- front-page.php
+++ front-page.php
@@ -1,19 +1,22 @@
-<p>front-page.php</p>
 <?php get_header(); ?>
-    <main>
+    <main class="main-front-page">
+        <!-- Section contenant le logo, la vidéo et la description -->
         <section class="hero">
-            <h1><?php echo get_bloginfo('name'); ?></h1>
-            <video src="">
+            <h1 class="titreLogo"><?php echo get_bloginfo('name'); ?></h1>
+            <video class="video-front-page" src="">
                 
             </video>
-            <h2>C'est quoi l'expo TIM?</h2>
-            <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
+            <div class="desc-expo">
+                <h2>C'est quoi l'expo TIM?</h2>
+                <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
+            </div>
+            
         </section>
         <section class="projet-random">
-
+            <h2>Choisis un projet au hasard</h2>
         </section>
         <section class="decks">
-
+            <h2>Nos decks de carte d’expositions</h2>
         </section>
     </main>
     <?php get_footer(); ?>
---
+++ functions/options.php
@@ -0,0 +1,30 @@
+<?php   
+    function mon_theme_supports() {
+        add_theme_support('title-tag');
+        add_theme_support('menus');
+        add_theme_support('post-thumbnails');
+        add_theme_support('custom-logo', array(
+          'height'      => 150,
+          'width'       => 150,
+          'flex-height' => true,
+          'flex-width'  => true,
+        ));
+      }
+      
+      add_action( 'after_setup_theme', 'mon_theme_supports' );
+
+
+
+      function theme_tp_enqueue_styles() { 
+        wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
+        wp_enqueue_style('main-style', get_stylesheet_uri()); 
+        // fichiers css
+        wp_enqueue_style('expo-header-style', get_template_directory_uri() . '/css/header.css');
+        wp_enqueue_style('expo-main-style', get_template_directory_uri() . '/css/main.css'); 
+        wp_enqueue_style('expo-front-page-style', get_template_directory_uri() . '/css/front-page.css'); 
+        
+        
+        
+    } 
+    add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
+?>
