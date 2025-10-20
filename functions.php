8c1d1592d76400914eb2eda7f161de92a20d81e9
massimo <masst123543@gmail.com>
2025-10-20 12:40:21 -0400

    linked css file

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@ function theme_tp_enqueue_styles() {
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
     wp_enqueue_style('main-style', get_stylesheet_uri()); 
     // fichiers css
-    wp_enqueue_style('style', get_template_directory_uri() . '/css/main.css'); 
-    wp_enqueue_style('gallerie', get_template_directory_uri() . '/css/gallerie.css'); 
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/main.css'); 
+    wp_enqueue_style('main-gallerie', get_template_directory_uri() . '/css/gallerie.css'); 
 } 
 add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 ?>
