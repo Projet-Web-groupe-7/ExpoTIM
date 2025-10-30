c8b2c0e2df20bdc5c5394375b99b6e5c11d0da5d
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-10-29 22:42:45 -0400

    Corrections header et deck de cartes foncitonnel page d'accueil

--- front-page.php
+++ front-page.php
@@ -1,20 +1,20 @@
 <h1 class="titreLogo"><?php echo get_bloginfo('name'); ?></h1>
+<video class="video-front-page" src="">
+                
+</video>
 <?php get_header(); ?>
     <main class="main-front-page">
         <!-- Section contenant le logo, la vidéo et la description -->
         <section class="hero">
-            <video class="video-front-page" src="">
-                
-            </video>
             <div class="desc-expo">
                 <h2>C'est quoi l'expo TIM?</h2>
                 <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
                 <div class="fondCarteAS"></div>
                 <div class="motifs">
-                    <img src="../images/cartes/clubs.png" alt="motifs">
+                    <!-- <img src="../images/cartes/clubs.png" alt="motifs">
                     <img src="../images/cartes/diamonds.png" alt="motifs">
                     <img src="../images/cartes/spades.png" alt="motifs">
-                    <img src="../images/cartes/hearts.png" alt="motifs">
+                    <img src="../images/cartes/hearts.png" alt="motifs"> -->
                 </div>
             </div>     
         </section>
@@ -24,7 +24,12 @@
         </section>
         <section class="decks">
             <h2>Nos decks de carte d’expositions</h2>
-            <details>
+            <?php wp_nav_menu(array(
+                    "menu" => "exposition",
+                    'container' => 'nav',
+                    'container_class' => 'expo-menu'
+                )); ?>
+            <!-- <details>
                 <summary>Finissants</summary>
                 <img src="" alt="">
             </details>
@@ -35,7 +40,7 @@
             <details>
                 <summary>Jour de la terre</summary>
                 <img src="" alt="">
-            </details>
+            </details> -->
             <div class="deckBox"></div>
         </section>
     </main>
