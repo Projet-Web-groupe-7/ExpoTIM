3ac117c6286aa9cbd3f004923062f8e41d2b5e33
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-11-04 16:04:53 -0500

    ajustement du css pour les decks de cartes

--- front-page.php
+++ front-page.php
@@ -4,44 +4,30 @@
 </video>
 <?php get_header(); ?>
     <main class="main-front-page">
-        <!-- Section contenant le logo, la vidéo et la description -->
-        <section class="hero">
-            <div class="desc-expo">
-                <h2>C'est quoi l'expo TIM?</h2>
-                <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
-                <div class="fondCarteAS"></div>
-                <div class="motifs">
-                    <!-- <img src="../images/cartes/clubs.png" alt="motifs">
-                    <img src="../images/cartes/diamonds.png" alt="motifs">
-                    <img src="../images/cartes/spades.png" alt="motifs">
-                    <img src="../images/cartes/hearts.png" alt="motifs"> -->
-                </div>
-            </div>     
+        <!-- Section contenant la description de l'exposition -->
+        <section class="desc-expo">
+            <h2>C'est quoi l'expo TIM?</h2>
+            <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
+            <div class="fondCarteAS"></div>
+            <div class="motifs">
+                <!-- <img src="../images/cartes/clubs.png" alt="motifs">
+                <img src="../images/cartes/diamonds.png" alt="motifs">
+                <img src="../images/cartes/spades.png" alt="motifs">
+                <img src="../images/cartes/hearts.png" alt="motifs"> -->
+            </div>
         </section>
+        <!-- Section contenant la sélection d'un projet aléatoire -->
         <section class="projet-random">
             <h2>Choisis un projet au hasard</h2>
             <img src=".images/cartes/carteDosR" alt="Carte">
         </section>
-        <section class="decks">
-            <h2>Nos decks de carte d’expositions</h2>
+        <section class="decks-expo">
+            <h2>Nos decks de cartes d’expositions</h2>
             <?php wp_nav_menu(array(
-                    "menu" => "exposition",
-                    'container' => 'nav',
-                    'container_class' => 'expo-menu'
-                )); ?>
-            <!-- <details>
-                <summary>Finissants</summary>
-                <img src="" alt="">
-            </details>
-            <details>
-                <summary>Arcade</summary>
-                <img src="" alt="">
-            </details>
-            <details>
-                <summary>Jour de la terre</summary>
-                <img src="" alt="">
-            </details> -->
-            <div class="deckBox"></div>
+                "menu" => "exposition",
+                'container' => 'nav',
+                'container_class' => 'expo-menu'
+            )); ?>      
         </section>
     </main>
     <?php get_footer(); ?>
