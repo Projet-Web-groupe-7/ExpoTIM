11c1c2aeff9a1daa40468267543eea1f1e558bcb
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-11-07 17:47:13 -0500

    Ajustements page d'accueil
    Deck de cartes d'exposition fonctionnel et amélioré + ajout de logo et images

---
+++ credits.php
@@ -0,0 +1,11 @@
+<main class="credits">
+  <section class="équipe">
+    <h2>Équipe de l'exposition TIM</h2>
+    <ul>
+      <li>Valérie Giroux-Martone</li>
+      <li>Sophie Nadeau</li>
+      <li>Erik Angel-Perez</li>
+      <li>Massimo Trimboli</li>
+    </ul>
+  </section>
+</main>
--- front-page.php
+++ front-page.php
@@ -8,7 +8,6 @@
         <section class="desc-expo">
             <h2>C'est quoi l'expo TIM?</h2>
             <p><?php echo get_theme_mod('hero_desc', __('Bienvenue sur le site de l\'exposition des travaux des étudiants du programme TIM', 'theme_5w5')); ?></p>
-            <div class="fondCarteAS"></div>
             <div class="motifs">
                 <!-- <img src="../images/cartes/clubs.png" alt="motifs">
                 <img src="../images/cartes/diamonds.png" alt="motifs">
@@ -23,11 +22,18 @@
         </section>
         <section class="decks-expo">
             <h2>Nos decks de cartes d’expositions</h2>
-            <?php wp_nav_menu(array(
-                "menu" => "exposition",
-                'container' => 'nav',
-                'container_class' => 'expo-menu'
-            )); ?>      
+            <!--  Boîte décorative -->
+            <div class="expo-deck">
+                <span class="expo-boite"></span>
+                <?php 
+                    wp_nav_menu(array(
+                        'menu' => 'exposition',
+                        'container' => 'nav',
+                        'container_class' => 'expo-menu',
+                        'menu_class' => 'deck-cartes', // Classe CSS pour l'élément <ul>
+                    )); 
+                ?>
+            </div>
         </section>
     </main>
     <?php get_footer(); ?>
