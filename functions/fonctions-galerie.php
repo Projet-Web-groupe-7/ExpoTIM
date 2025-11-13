bbf8765638b01e45386e43bdfb8547833bcb773f
massimo <masst123543@gmail.com>
2025-11-13 16:47:56 -0500

    mise à jour (de mes parties) avec le distant

--- functions/fonctions-galerie.php
+++ functions/fonctions-galerie.php
@@ -11,10 +11,10 @@
                 );
             break;
 
-            case 'jour-de-la-terre':
+            case 'graphisme':
                 $args =  array(
                     'posts_per_page'  => -1,
-                    'post_type'       => 'projets-jour-terre',
+                    'post_type'       => 'projets-graphisme',
                 );
             break;
         }
@@ -23,7 +23,7 @@
 
     // ne pas oublier de rajouter les cas ici, necessaire pour
     // la fonction section_projet_random()
-    $allCases = ['arcade', 'jour-de-la-terre'];
+    $allCases = ['arcade', 'graphisme'];
 
 
 
@@ -71,9 +71,9 @@
                     $post_img = get_field('projet-arcade_image');
                 break;
 
-                case 'jour-de-la-terre':
-                    $post_name = get_field('projet-jour-terre_nom');
-                    $post_img = get_field('projet-jour-terre_image');
+                case 'graphisme':
+                    $post_name = get_field('projet-graphisme_nom');
+                    $post_img = get_field('projet-graphisme_image');
                 break;
             }
     ?>
