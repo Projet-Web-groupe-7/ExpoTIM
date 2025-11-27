9a49c919c7ce58d0a5f1fa406c6416be00b34dd9
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-11-27 18:54:41 -0500

    Correction ordre front-page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,13 @@
-<h1 class="titreLogo"><?php echo get_bloginfo('name'); ?></h1>
-<div class="contenuVideo">
-<video autoplay loop muted playsinline class="video-front-page">
-        <source src="<?php echo get_template_directory_uri(); ?>/video/VideoPromotionnelleWeb.mp4" type="video/mp4">
-    </video> 
-</div> 
 <?php get_header(); ?>
     <main class="main-front-page">
+        <section class="hero">
+            <h1 class="titreLogo"><?php echo get_bloginfo('name'); ?></h1>
+            <div class="contenuVideo">
+                <video autoplay loop muted playsinline class="video-front-page">
+                    <source src="<?php echo get_template_directory_uri(); ?>/video/VideoPromotionnelleWeb.mp4" type="video/mp4">
+                </video> 
+            </div> 
+        </section>
         <!-- Section contenant la description de l'exposition -->
         <section class="desc-expo">
             <h2>C'est quoi l'expo TIM?</h2>
